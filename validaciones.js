80d96990a319f1b16b0d29e5439f656ea9085854
JuliMart <juli.martinezm@duocuc.cl>
2023-05-14 04:05:11 -0400

    formulario agregar vehiculos

---
+++ validaciones.js
@@ -0,0 +1,17 @@
+function valida(){
+    var formularioOk = true
+
+    var marca = $("#marca").val()
+    var largoMarca = marca.length
+        if(largoMarca < 4 || largoMarca > 10){
+            formularioOk = false
+            $("#msgmarca").show()
+        }
+
+        var modelo = $("#modelo").val()
+        var largoModelo = modelo.length
+            if(largoModelo < 4 || largoModelo > 10){
+                formularioOk = false
+                $("#msgmodelo").show()
+            }
+}
