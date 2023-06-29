3bb722923f50eb3846436d19d318a121b86954ac
Fernanda Garay <m.garay@duocuc.cl>
2023-06-29 18:05:20 -0400

    Agrega proyecto springboot
    Se crea proyecto springboot y se mueven archivos web dentro de carpeta resources/public

---
+++ src/test/java/cl/duoc/newrentacar/NewRentACarApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.newrentacar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewRentACarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
