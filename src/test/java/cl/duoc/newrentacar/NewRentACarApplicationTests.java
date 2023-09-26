5ac328e71a5b57929f7c1a8ddf73f1acbcca4614
ctronp <carloscasa7@outlook.cl>
2023-09-26 19:59:29 -0300

    Initial commit test.

--- src/test/java/cl/duoc/newrentacar/NewRentACarApplicationTests.java
+++ src/test/java/cl/duoc/newrentacar/NewRentACarApplicationTests.java
@@ -3,11 +3,14 @@ package cl.duoc.newrentacar;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 class NewRentACarApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
 
 }
