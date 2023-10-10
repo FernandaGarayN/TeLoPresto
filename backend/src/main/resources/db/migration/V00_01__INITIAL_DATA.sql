f6fad6fedea7913fc0625f58a171814b15f34aff
ctronp <carloscasa7@outlook.cl>
2023-10-09 23:22:18 -0300

    Initialize database with cars and users tables
    This commit introduces new tables 'cars' and 'users' along with their corresponding initial data. 'cars' table represents available cars and their details while 'users' table holds user credentials for authentication. Data of few cars and users are also added as part of the initialization. Moreover, flyway dependency has been added for database migrations and jpa property 'ddl-auto' is set to 'validate' to confirm the database structure at startup.

---
+++ backend/src/main/resources/db/migration/V00_01__INITIAL_DATA.sql
@@ -0,0 +1,14 @@
+INSERT INTO cars (plate_code, brand, model, color, factory_year, capacity, daily_cost, type )
+VALUES ('BBCC00', 'Chevrolet', 'Sail', 'Black', '2018', '5', '70000', 'Sedan'),
+       ('ZZYY99', 'Chevrolet', 'Sail', 'White', '2020', '5', '70000','Sedan'),
+       ('YYZZ00', 'Hyundai', 'Elantra', 'Blue', '2012', '5', '50000', 'Sedan'),
+       ('RTXT65', 'Chery', 'Tiggo3', 'Grey', '2022', '5', '90000', 'SUV'),
+       ('RTXT65', 'Lego', 'Praia', 'Red', '2012', '2', '100000', 'SUV'),
+       ('RTXT65', 'Nissan', 'Pathfinder', 'Grey', '2018', '7', '60000', 'SUV'),
+       ('RTXT65', 'Chevrolet', 'V34', 'Blue', '2023', '4', '30000', 'SUV');
+
+INSERT INTO users (email,password )
+VALUES ('rodr.ulloa@duocuc.cl', '1234' ),
+       ('juli.martinezm@duocuc.cl', '4321'),
+       ('fr.moralesc@duocuc.cl', '2023'),
+       ('m.garay@duocuc.cl', '1111');
