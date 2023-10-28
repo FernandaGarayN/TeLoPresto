1e567c7ac3b50fd0a4c312762d7ccac778be3870
ctronp <carloscasa7@outlook.cl>
2023-10-28 01:33:36 -0300

    Refactor PaymentService.java to remove unnecessary imports
    Removed unnecessary imports in PaymentService.java, specifically Car, Reservation and their respective Entity and Repository classes. These were eliminated as they were not being utilized in the file, thus reducing clutter and improving readability. Simplified indentation for better code consistency.

--- backend/src/main/java/cl/duoc/newrentacar/api/service/PaymentService.java
+++ backend/src/main/java/cl/duoc/newrentacar/api/service/PaymentService.java
@@ -1,13 +1,8 @@
 package cl.duoc.newrentacar.api.service;
 
-import cl.duoc.newrentacar.api.endpoint.model.Car;
 import cl.duoc.newrentacar.api.endpoint.model.Payment;
-import cl.duoc.newrentacar.api.endpoint.model.Reservation;
 import cl.duoc.newrentacar.api.repository.PaymentRepository;
-import cl.duoc.newrentacar.api.repository.ReservationRepository;
-import cl.duoc.newrentacar.api.repository.model.CarEntity;
 import cl.duoc.newrentacar.api.repository.model.PaymentEntity;
-import cl.duoc.newrentacar.api.repository.model.ReservationEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,29 +11,30 @@ import java.util.List;
 
 @Service
 public class PaymentService {
-    @Autowired
-    private PaymentRepository paymentRepository;
-    public List<Payment> findByUserName(String userName){
-        List<PaymentEntity> paymentsByUserName=paymentRepository.findByClientUsername(userName);
-        List<Payment> payments= new ArrayList<>();
-        for (PaymentEntity entity : paymentsByUserName) {
-            payments.add(getPayment(entity));
-        }
-        return payments;
-    }
+  @Autowired
+  private PaymentRepository paymentRepository;
 
-    private Payment getPayment(PaymentEntity entity) {
-        Payment payment=new Payment();
-        payment.setId(entity.getId());
-        payment.setReservationId(entity.getReservation());
-        payment.setType(getType(entity.getType()));
-        payment.setAmount(entity.getAmount());
-        payment.setPaymentDate(entity.getPaymentDate());
-        return payment;
+  public List<Payment> findByUserName(String userName) {
+    List<PaymentEntity> paymentsByUserName = paymentRepository.findByClientUsername(userName);
+    List<Payment> payments = new ArrayList<>();
+    for (PaymentEntity entity : paymentsByUserName) {
+      payments.add(getPayment(entity));
     }
+    return payments;
+  }
 
-    public Payment save(Payment payment) {
-        //agregar la logica para almacenar en BD con reservation repository
-        return payment;
-    }
+  private Payment getPayment(PaymentEntity entity) {
+    Payment payment = new Payment();
+    payment.setId(entity.getId());
+    payment.setReservationId(entity.getReservation().getId());
+    payment.setType(entity.getType());
+    payment.setAmount(entity.getAmount());
+    payment.setPaymentDate(entity.getPaymentDate());
+    return payment;
+  }
+
+  public Payment save(Payment payment) {
+    //agregar la logica para almacenar en BD con reservation repository
+    return payment;
+  }
 }
